fe1cc1d3f1cc49703fe6bc6ba513d592c3a153ec
vrill <52007698+vrill@users.noreply.github.com>
2022-02-06 16:49:22 +0100

    Created the userschema and model, and set up a new endpoint for the signup.

--- backend/server.js
+++ backend/server.js
@@ -4,7 +4,28 @@ import mongoose from 'mongoose'
 
 const mongoUrl = process.env.MONGO_URL || 'mongodb://localhost/authAPI'
 mongoose.connect(mongoUrl, { useNewUrlParser: true, useUnifiedTopology: true })
+mongoose.set('useCreateIndex', true)
 mongoose.Promise = Promise
+
+const crypto = require('crypto')
+
+const UserSchema = new mongoose.Schema({
+    username: {
+        type: String,
+        unique: true,
+        required: true,
+    },
+    password: {
+        type: String,
+        required: true,
+    },
+    accessToken: {
+        type: String,
+        default: () => crypto.randomBytes(128).toString('hex')
+    }
+})
+
+const User = mongoose.model('User', UserSchema)
 
 const port = process.env.PORT || 8080
 const app = express()
@@ -14,6 +35,16 @@ app.use(express.json())
 
 app.get('/', (req, res) => {
     res.send('Up and running.')
+})
+
+app.post('/signup', async (req, res) => {
+    const { username, password } = req.body
+
+    try {
+        
+    } catch (error) {
+
+    }
 })
 
 app.listen(port, () => {
