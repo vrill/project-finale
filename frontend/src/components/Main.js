06d9393c93645b68ed3aad02d1e51866c2072467
vrill <52007698+vrill@users.noreply.github.com>
2022-02-10 21:48:02 +0100

    Updated the Main.js to handle login & prepared to display plants.

--- frontend/src/components/Main.js
+++ frontend/src/components/Main.js
@@ -1,8 +1,34 @@
-import React from "react"
+import React, { useEffect } from 'react'
+import { useSelector } from 'react-redux'
+
+import { API_URL } from '../utils/constants'
 
 const Main = () => {
+
+    const plantsItems = useSelector((store) => store.plants.items)
+    const accessToken = useSelector((store) => store.user.accessToken)
+    
+    const options = {
+        method: 'GET',
+        headers: {
+            'Authorization': accessToken
+        }
+    }
+
+    useEffect(() => {
+        fetch(API_URL('plants'), options)
+            .then((response) => response.json())
+            .then((data) => console.log(data))
+    }, [])
+
     return (
-        <h1>Main</h1>
+        <div>
+            <h1>Only for logged in users.</h1>
+            <h2>Plants:</h2>
+            {plantsItems.map(item => (
+                <div key={item._id}>{item.message}</div>
+            ))}
+        </div>
     )
 }
 
