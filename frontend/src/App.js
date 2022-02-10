371d1dbbef60db8f376a373941c0094385a63e54
vrill <52007698+vrill@users.noreply.github.com>
2022-02-10 20:39:21 +0100

    Updated the App component to import needed files, added a reducer and first draft of the structure/routes.

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,36 @@
 import React from 'react'
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import { 
+    BrowserRouter,
+    Routes,
+    Route
+} from 'react-router-dom'
+import { Provider } from 'react-redux'
+import { configureStore, combineReducers } from '@reduxjs/toolkit'
+
+import Login from './components/Login'
+import Main from './components/Main'
+import NotFound from './components/NotFound'
+
+import user from './reducers/user'
+import plants from './reducers/plants'
+
+const reducer = combineReducers({
+    user: user.reducer,
+    plants: plants.reducer
+})
+
+const store = configureStore({ reducer })
 
 export const App = () => {
     return (
-        <BrowserRouter>
-            <Routes>
-                <Route />
-            </Routes>
-        </BrowserRouter>
+        <Provider store={store}>
+            <BrowserRouter>
+                <Routes>
+                    <Route path="/" element={<Main />} />
+                    <Route path="/login" element={<Login />} />
+                    <Route path="*" element={<NotFound />} />
+                </Routes>
+            </BrowserRouter>
+        </Provider>
     )
 }
