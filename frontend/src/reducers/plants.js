7ea9d8088f8d5e028b7fd85af835ac3490894447
vrill <52007698+vrill@users.noreply.github.com>
2022-02-18 12:29:01 +0100

    Updated the plants.js reducer to add, toggle and delete plants/items. Based on previous todo project.

--- frontend/src/reducers/plants.js
+++ frontend/src/reducers/plants.js
@@ -1,4 +1,5 @@
 import { createSlice } from '@reduxjs/toolkit'
+import uniqid from 'uniqid'
 
 const plants = createSlice({
     name: 'plants',
@@ -7,8 +8,37 @@ const plants = createSlice({
         error: null,
     },
     reducers: {
-        setItems: (store, action) => {
-            store.items = action.payload
+        addPlant: (store, action) => {
+            const data = action.payload
+
+            const newPlant = {
+                id: uniqid(),
+                text: data,
+                isComplete: false,
+            }
+            store.items = [...store.items, newPlant]
+        },
+        togglePlant: (store, action) => {
+            const updatedItems = store.items.map((item) => {
+                if (item.id === action.payload) {
+                    const updatedPlant = {
+                        ...item,
+                        icComplete: !item.isComplete,
+                    }
+                    return updatedPlant
+                } else {
+                    return item
+                }
+            })
+            store.items = updatedItems
+        },
+        deletePlant: (store, action) => {
+            const decreasedItems = store.items.filter((item) => item.id !== action.payload)
+
+            store.items = decreasedItems
+        },
+        deleteAllPlants: (store) => {
+            store.items = []
         },
         setError: (store, action) => {
             store.error = action.payload
