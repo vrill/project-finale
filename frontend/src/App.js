71c6b236d4df5965f21b156f9207015c9ef9a0bf
vrill <52007698+vrill@users.noreply.github.com>
2022-02-19 00:51:08 +0100

    Removed a bunch of decommented code.

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,5 @@
 import React from 'react'
-import { 
-    BrowserRouter,
-    Routes,
-    Route
-} from 'react-router-dom'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import { Provider } from 'react-redux'
 import { configureStore, combineReducers } from '@reduxjs/toolkit'
 
--- frontend/src/components/Main.js
+++ frontend/src/components/Main.js
@@ -1,9 +1,6 @@
 import React, { useEffect } from 'react'
-import { useDispatch, useSelector } from 'react-redux'
-import { useNavigate, Link } from 'react-router-dom'
-
-// import plants from '../reducers/plants'
-// import { API_URL } from '../utils/constants'
+import { useSelector } from 'react-redux'
+import { useNavigate } from 'react-router-dom'
 
 import Header from './Header'
 import AddPlant from './AddPlant'
@@ -12,10 +9,8 @@ import PlantList from './PlantList'
 
 const Main = () => {
 
-    // const plantsItems = useSelector((store) => store.plants.items)
     const accessToken = useSelector((store) => store.user.accessToken)
 
-    // const dispatch = useDispatch()
     const navigate = useNavigate()
 
     useEffect(() => {
@@ -24,39 +19,8 @@ const Main = () => {
         }
     }, [accessToken, navigate])
 
-    // useEffect(() => {
-    //     const options = {
-    //         method: 'GET',
-    //         headers: {
-    //             'Authorization': accessToken
-    //         }
-    //     }
-
-    //     fetch(API_URL('plants'), options)
-    //         .then((response) => response.json())
-    //         .then((data) => {
-    //             if (data.success) {
-    //                 dispatch(plants.actions.setItems(data.response))
-    //                 dispatch(plants.actions.setError(null))
-    //             } else {
-    //                 dispatch(plants.actions.setItems([]))
-    //                 dispatch(plants.actions.setError(data.response))
-    //             }
-    //         })
-    // }, [accessToken])
-
     return (
         <>
-            {/* <div>
-                <div>
-                    <Link to="/login">To login portal.</Link>
-                </div>
-                <h1>Only for logged in users.</h1>
-                <h2>Plants:</h2>
-                {plantsItems.map(item => (
-                    <div key={item._id}>{item.message}</div>
-                ))}
-            </div> */}
             <Header />
             <AddPlant />
             <PlantList />
